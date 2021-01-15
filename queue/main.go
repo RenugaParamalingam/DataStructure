d6f6be2ba7a95d561bbd6a477985af0231dadf2e
RenugaParamalingam <renumpk@gmail.com>
2021-01-15 12:49:10 +0530

    queue

---
+++ queue/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	var q Queue
+	q.Enqueue("1")
+	q.Print()
+
+	q.Dequeue()
+	q.Print()
+
+	q.Dequeue()
+}
+
+// Queue structure
+type Queue []string
+
+// Enqueue inserts element to the queue
+func (q *Queue) Enqueue(element string) {
+	*q = append(*q, element)
+	fmt.Println("Enqueued: ", element)
+}
+
+// Dequeue removes element in front of queue
+func (q *Queue) Dequeue() {
+	if q.Length() < 1 {
+		fmt.Println("Queue is empty. Can't deuqueue.")
+
+		return
+	}
+
+	fmt.Println("Dequeued: ", (*q)[0])
+	(*q) = (*q)[1:]
+}
+
+// Print prints all elements in queue
+func (q *Queue) Print() {
+	fmt.Println("Elements in queue")
+
+	for _, v := range *q {
+		fmt.Println(v)
+	}
+}
+
+// Length returns length of queue
+func (q *Queue) Length() int {
+	return len(*q)
+}
