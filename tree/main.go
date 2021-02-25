552885ce28411e721e6d66a164629e6ed658e282
Renuga <renumpk@gmail.com>
2021-02-25 13:03:36 +0530

    min depth

--- tree/main.go
+++ tree/main.go
@@ -31,7 +31,7 @@ func main() {
 	fmt.Println("\n Get level order: ", t.GetLevelOrder())
 
 	fmt.Println("max depth: ", MaxDepth(t.Root))
-
+	fmt.Println("min depth: ", MinDepth(t.Root))
 }
 
 // Node holds structure for a node in binary tree
@@ -179,6 +179,30 @@ func MaxDepth(root *Node) int {
 
 func max(a int, b int) int {
 	if a > b {
+		return a
+	}
+	return b
+}
+
+// MinDepth returns minimum depth of the tree.
+func MinDepth(root *Node) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil {
+		return MinDepth(root.Right) + 1
+	}
+
+	if root.Right == nil {
+		return MinDepth(root.Left) + 1
+	}
+
+	return min(MinDepth(root.Left), MinDepth(root.Right)) + 1
+}
+
+func min(a int, b int) int {
+	if a < b {
 		return a
 	}
 	return b
